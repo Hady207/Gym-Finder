ab1eed1ffdb6d80ca776d28ec684aed0cfdf09e1
Hadi Maher <hady207@hotmail.com>
2020-05-03 05:21:53 +0300

    Finshed signup markup

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 function Signup() {
+  const [formNum, setNum] = useState(0);
   return (
     <>
       <Nav regular />
@@ -17,7 +18,12 @@ function Signup() {
               <div className="signup__circle signup__circle--3">3</div>
             </div>
             <div className="signup__grid--right">
-              <div className="signup__inputs signup__inputs--1">
+              {/* First sign up group */}
+              <div
+                className={`signup__inputs signup__inputs--1 ${
+                  formNum === 0 ? "signup__show" : ""
+                } `}
+              >
                 <div className="signup__group">
                   <label for="fname">First Name: </label>
                   <input
@@ -54,6 +60,103 @@ function Signup() {
                   <input type="text" name="address" id="address" />
                   <label for="phone">Phone: </label>
                   <input type="phone" name="phone" id="phone" />
+                </div>
+                <div className="signup__button">
+                  <button
+                    onClick={() => setNum((n) => n + 1)}
+                    className="signup__button--next"
+                  >
+                    Next <i class="fas fa-angle-right"></i>
+                  </button>
+                </div>
+              </div>
+              {/* Second sign up group */}
+              <div
+                className={`signup__inputs signup__inputs--2 ${
+                  formNum === 1 ? "signup__show" : ""
+                }`}
+              >
+                <div className="signup__group">
+                  <label for="email">Email:</label>
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="doe@example.com"
+                  />
+                </div>
+                <div className="signup__group">
+                  <label for="password">Password:</label>
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+                <div className="signup__group">
+                  <label for="rpassword">Repeat Password:</label>
+                  <input
+                    id="rpassword"
+                    type="password"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+                <div className="signup__group">
+                  <div className="signup__profilePicture">
+                    <img
+                      src={require("../src/img/default.jpg")}
+                      alt="Profile"
+                    />
+                  </div>
+                  <input type="file" id="myFile" name="filename" />
+                </div>
+                <div className="signup__buttons">
+                  <button
+                    onClick={() => setNum((n) => n - 1)}
+                    className="signup__button--prev"
+                  >
+                    <i class="fas fa-angle-left"></i> Prev
+                  </button>
+                  <button
+                    onClick={() => setNum((n) => n + 1)}
+                    className="signup__button--next"
+                  >
+                    Next <i class="fas fa-angle-right"></i>
+                  </button>
+                </div>
+              </div>
+              {/* Third sign up group */}
+              <div
+                className={`signup__inputs signup__inputs--3${
+                  formNum === 2 ? "signup__show" : ""
+                }`}
+              >
+                <div className="signup__question">
+                  <p>Have you ever went to these gyms ?</p>
+                  <select required id="gym">
+                    <option>Golds Gym</option>
+                    <option>Iron Man</option>
+                    <option>Flex</option>
+                    <option>Fitness first me</option>
+                  </select>
+                </div>
+                <div className="signup__group">
+                  <label for="weight">Weight</label>
+                  <input type="number" id="weight" />
+                </div>
+                <div className="signup__group">
+                  <label for="height">Height</label>
+                  <input type="number" id="height" />
+                </div>
+                <div className="signup__buttons">
+                  <button
+                    onClick={() => setNum((n) => n - 1)}
+                    className="signup__button--prev"
+                  >
+                    <i class="fas fa-angle-left"></i> Prev
+                  </button>
+                  <button className="signup__button--next">Signup</button>
                 </div>
               </div>
             </div>
