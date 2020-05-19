a1440f36bab3c4dd463535ba02d976f7c860b82a
Hadi Maher <hady207@hotmail.com>
2020-05-19 05:02:40 +0300

    refactor signup1

--- src/Signup Components/Signup1.js
+++ src/Signup Components/Signup1.js
@@ -12,51 +12,63 @@ function Signup1({ formNum, setNum, manage }) {
       } `}
     >
       <div className="signup__group">
-        <label htmlFor="fname">First Name: </label>
-        <input
-          required
-          type="text"
-          placeholder="Ahmad"
-          name="firstName"
-          id="fname"
-          onChange={onChange}
-        />
-        <label htmlFor="lname">Last Name: </label>
-        <input
-          required
-          placeholder="Maher"
-          type="text"
-          name="lastName"
-          id="lname"
-          onChange={onChange}
-        />
+        <div className="signup__subgroup">
+          <label htmlFor="fname">First Name </label>
+          <input
+            required
+            type="text"
+            placeholder="Ahmad"
+            name="firstName"
+            id="fname"
+            onChange={onChange}
+          />
+        </div>
+        <div className="signup__subgroup">
+          <label htmlFor="lname">Last Name </label>
+          <input
+            required
+            placeholder="Maher"
+            type="text"
+            name="lastName"
+            id="lname"
+            onChange={onChange}
+          />
+        </div>
       </div>
       <div className="signup__group">
-        <label required htmlFor="birthdate">
-          Birthday:
-        </label>
-        <input
-          type="date"
-          name="birthDate"
-          id="birthdate"
-          onChange={onChange}
-        />
-        <label htmlFor="city">City: </label>
-        <select name="city" onChange={onChange} required id="city">
-          <option defaultValue disabled>
-            Address
-          </option>
-          <option value="Salmiya">Salmiya</option>
-          <option value="Hawally">Hawally</option>
-          <option value="Farawanya">Farawanya</option>
-          <option value="Jahra">Jahra</option>
-        </select>
+        <div className="signup__subgroup">
+          <label required htmlFor="birthdate">
+            Birthday
+          </label>
+          <input
+            type="date"
+            name="birthDate"
+            id="birthdate"
+            onChange={onChange}
+          />
+        </div>
+        <div className="signup__subgroup">
+          <label htmlFor="city">City </label>
+          <select name="city" onChange={onChange} required id="city">
+            <option defaultValue disabled>
+              Address
+            </option>
+            <option value="Salmiya">Salmiya</option>
+            <option value="Hawally">Hawally</option>
+            <option value="Farawanya">Farawanya</option>
+            <option value="Jahra">Jahra</option>
+          </select>
+        </div>
       </div>
       <div className="signup__group">
-        <label htmlFor="address">Address: </label>
-        <input onChange={onChange} type="text" name="address" id="address" />
-        <label htmlFor="phone">Phone: </label>
-        <input onChange={onChange} type="phone" name="phone" id="phone" />
+        <div className="signup__subgroup">
+          <label htmlFor="address">Address </label>
+          <input onChange={onChange} type="text" name="address" id="address" />
+        </div>
+        <div className="signup__subgroup">
+          <label htmlFor="phone">Phone </label>
+          <input onChange={onChange} type="phone" name="phone" id="phone" />
+        </div>
       </div>
       <div className="signup__button">
         <button
