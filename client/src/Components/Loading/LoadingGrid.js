c04156c474ee371d8450ea68f69e23b1d73e36fe
Hadi Maher <hady207@hotmail.com>
2020-07-26 01:06:40 +0300

    Fixed live css compiler location, finished the loading indecator in gyms page

---
+++ client/src/Components/Loading/LoadingGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingGrid = () => {
+  return (
+    <div className="lds-dual-ring-container">
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+};
+
+export default LoadingGrid;
--- client/src/pages/Gyms.js
+++ client/src/pages/Gyms.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 import useInputReducer from '../hooks/useInputReducer';
 import GymCard from '../Components/Gym Components/GymCard';
 import Searchtools from '../Components/Gyms Components/Searchtools';
 import Gymtype from '../Components/Gyms Components/Gymtype';
+import LoadingGrid from '../Components/Loading/LoadingGrid';
 
 const Gyms = (props) => {
   const initVal = {
@@ -26,10 +28,12 @@ const Gyms = (props) => {
   };
   const [searchValues, dispatch] = useInputReducer(initVal);
   const [gyms, setGyms] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       const gymsData = await axios.get('/api/v1/gyms');
       setGyms((st) => [...st, ...gymsData.data.data.gyms]);
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -44,17 +48,25 @@ const Gyms = (props) => {
           </div>
           <div className="gyms__grid">
             {/* grid here */}
-            <div className="gym__innerGrid">
-              {/* <!-- card start here --> */}
-              {gyms.map((item) => (
-                <GymCard data={item} />
-              ))}
-              {/* <GymCard />
+            {loading ? (
+              <LoadingGrid />
+            ) : (
+              <div className="gym__innerGrid">
+                {/* <!-- card start here --> */}
+                {gyms.map((item) => (
+                  <GymCard data={item} />
+                ))}
+                {/* <!-- card end here --> */}
+              </div>
+            )}
+            {/* <div className="gym__innerGrid"> */}
+            {/* <!-- card start here --> */}
+            {/* <GymCard />
               <GymCard />
               <GymCard />
               <GymCard /> */}
-              {/* <!-- card end here --> */}
-            </div>
+            {/* <!-- card end here --> */}
+            {/* </div> */}
           </div>
         </div>
       </main>
